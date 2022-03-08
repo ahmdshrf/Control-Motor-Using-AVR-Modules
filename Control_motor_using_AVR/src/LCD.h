271d12215d3deb6260399739095716c8b9e1c239
Ahmed Sherif <36515991+ahmdshrf@users.noreply.github.com>
2022-03-08 02:08:46 +0200

    Update LCD.h

--- Control_motor_using_AVR/src/LCD.h
+++ Control_motor_using_AVR/src/LCD.h
@@ -6,8 +6,6 @@
  *
  * Description: Header file for the LCD driver
  *
- * Author: Mohamed Tarek
- *
  *******************************************************************************/
 
 #ifndef LCD_H_
